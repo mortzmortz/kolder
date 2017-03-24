58c0ac0f611ce2195b4e3aa3e1f3cebfc60081d3
Gerion Herbst <gherbst@3pc.de>
2017-03-24 10:36:43 +0100

    update help command

--- bin/kolder.js
+++ bin/kolder.js
@@ -4,11 +4,17 @@ const chalk = require('chalk');
 const pkg = require('../package');
 
 const argv = yargs
-  .usage('\nkolder <project-name>')
+  .usage('\nkolder <project-name> <type>')
   .version(pkg.version)
+  .command(
+    'project-name', 
+    `<project-name> is the directory kolder will be initialized in.
+    If no directory is provided, the current directory will be asumed.`
+  )
+  .command('type', `<type> is the project type. You can skip prompts with providing --type=plain.`)
   .alias('h', 'help')
   .alias('v', 'version')
-  .epilogue('for more information, visit https://github.com/mortzmortz/koldere')
+  .epilogue('for more information, visit https://github.com/mortzmortz/kolder')
   .help()
   .argv;
 
